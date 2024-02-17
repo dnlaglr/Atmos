3bdbe21338cfab3f8a258e66e0316aa62057bba1
DanielAglr <155811921+dnlaglr@users.noreply.github.com>
2024-02-17 02:39:59 -0800

    Create deployment endpoint

--- atmos/server/index.ts
+++ atmos/server/index.ts
@@ -2,27 +2,41 @@ import express from 'express'
 import cors from 'cors'
 import simpleGit from 'simple-git'
 
-import { generateDeployID } from '../helper'
+import { deployDoesntExist, generateDeployID, isValidGitRepoURL } from '../helper'
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Hello Server!");
-});
+app.use(express.urlencoded({ extended: true }));
 
 app.post("/deploy", async (req, res) => {
   const repoURL = req.body.repoURL;
-  const deployID = generateDeployID(repoURL);
 
-  await simpleGit().clone(repoURL, `repos/${deployID}`);
+  if (repoURL) {
+    const deployID = generateDeployID(repoURL);
 
-  res.json({
-    deployID: deployID
-  });
+    if (isValidGitRepoURL(repoURL) && deployID) {
+      if (deployDoesntExist(deployID)) {
+        await simpleGit().clone(repoURL, `repos/${deployID}`);
+    
+        res.json({
+          deployID: deployID
+        });
+      } else {
+        res.json({
+          error: "Error: Deployment already exists."
+        });
+      }
+    } else {
+      res.json({
+        error: "Error: Not a valid GitHub repo URL."
+      });
+    }
+  } else {
+    console.error("Missing repoURL")
+  }
 });
 
 app.listen(5171, () => {
-  console.log("Server listening at http://localhost:5171");
+  console.log('Server listening at http://localhost:5171');
 });
